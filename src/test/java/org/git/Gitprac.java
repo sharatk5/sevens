fd19382bc608b552b170ebaf5df8e3fa3940898e
sharatk <sharat1102@gmail.com>
2023-02-04 10:32:22 +0530

    completed

---
+++ src/test/java/org/git/Gitprac.java
@@ -0,0 +1,21 @@
+package org.git;
+
+public class Gitprac {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String s= "345628";
+		
+		System.out.println(s.charAt(3));
+		
+		
+		
+	}
+	
+	
+	
+
+}
